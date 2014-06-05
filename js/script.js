a25887084821bd4672bf3edde010514db0a83113
EvelynLiu <100703007@nccu.edu.tw>
2014-06-05 14:25:49 +0800

    commit to gh-pages

--- js/script.js
+++ js/script.js
@@ -43,10 +43,13 @@ FB.getLoginStatus(function (response) {
 	ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); //設定預設的開始畫面
     var img = new Image(); // 新增圖像1
     img.src = "img/PinkFrame.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
+    img.width=546 px;
 	var img2 = new Image(); //新增圖像2
 	img2.src = "img/Red-rose.png" //圖像路徑
+	img2.width=546 px;
 	var img3 = new Image();//新增圖像3
 	img3.src = "img/wallpaper.png"//圖像路徑
+	img3.width=546 px;
 	
 	
 
@@ -91,9 +94,9 @@ FB.getLoginStatus(function (response) {
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			//canvas.height = profileIMG.height;
+			ctx.drawImage(img2,0,0); //劃入img2
 			ctx.drawImage(profileIMG,0,0);//從XY軸0，0值開始畫如profileimg
 			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-			ctx.drawImage(img2,0,0); //劃入img2
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
