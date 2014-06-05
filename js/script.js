7efc75964e16576f01f038dca74c16e409dc3a0c
EvelynLiu <100703007@nccu.edu.tw>
2014-06-05 15:18:30 +0800

    commit to gh-pages

--- js/script.js
+++ js/script.js
@@ -44,9 +44,9 @@ FB.getLoginStatus(function (response) {
     var img = new Image(); // 新增圖像1
     img.src = "img/PinkFrame.jpg"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
 	var img2 = new Image(); //新增圖像2
-	img2.src = "img/wallpaper.jpg" //圖像路徑
+	img2.src = "img/wall_img.jpg" //圖像路徑
 	var img3 = new Image();//新增圖像3
-	img3.src = "img/textbox.jpg"//圖像路徑
+	img3.src = "img/text.jpg"//圖像路徑
 	
 	
 
