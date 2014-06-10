6f086ad95bfe731acab5b5e8a96805e093f0f79f
EvelynLiu <100703007@nccu.edu.tw>
2014-06-10 16:39:13 +0800

    commit to gh-pages

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ FB.getLoginStatus(function (response) {
     FB.api("/me/picture?type=large",function (e){
     	$("#preview1").attr("src",e.data.url);
     });
-    FB.api("641547479267436?fields=comments",function (e){
+    FB.api("641547479267436",function (e){
     	//$('#infoOfPic').append();
     	console.log(e.data);
     });
