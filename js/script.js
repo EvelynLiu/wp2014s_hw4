0adce27ce9c2153700d12a52aa0d43efc4810bf4
EvelynLiu <100703007@nccu.edu.tw>
2014-06-11 16:37:07 +0800

    commit to gh-pages

--- js/script.js
+++ js/script.js
@@ -231,6 +231,18 @@ $("#pattern").change(function(){
 	image.src=e;
 });
 
+function handleFiles(e){
+	var t=document.getElementById("canvas").getContext("2d");
+	var n=URL.createObjectURL(e.target.files[0]);
+	image.src=n;
+}
+
+var files = false;
+window.onload=function(){
+	var e=document.getElementById("input");
+	e.addEventListener("change",handleFiles,false);
+}
+
 $("#album").change(function(){
 	var e=this.options[this.selectedIndex].value;
 	var t=e+"/photos";
@@ -264,19 +276,5 @@ $("#photo").change(function(){
 	})
 });
 
-/*function handleFiles(e){
-	var t=document.getElementById("canvas").getContext("2d");
-	var n=URL.createObjectURL(e.target.files[0]);
-	var r=new Image;
-	r.onload=function(){
-		t.drawImage(r,270/2,270/2)
-	};
-	r.src=n;
-	$("#canvas").css("pointer-events","none");
-}
 
-window.onload=function(){
-	var e=document.getElementById("input");
-	e.addEventListener("change",handleFiles,false)
-}*/
 
